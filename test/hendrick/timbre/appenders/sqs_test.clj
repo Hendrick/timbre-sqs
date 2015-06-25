7540a37617c36f7d71b0f5ecda7926e233bf263c
Larry Staton Jr <larry@larrystaton.com>
2015-06-25 02:36:52 +0000

    Update appender and tests to use latest changes to timbre.

--- test/hendrick/timbre/appenders/sqs_test.clj
+++ test/hendrick/timbre/appenders/sqs_test.clj
@@ -3,7 +3,7 @@
             [amazonica.core :refer [with-credential]]
             [amazonica.aws.sqs :as sqs]
             [taoensso.timbre :as timbre]
-            [hendrick.timbre.appenders.sqs :refer [make-sqs-appender]])
+            [hendrick.timbre.appenders.sqs :refer [sqs-appender]])
   (:import [org.elasticmq.rest.sqs SQSRestServerBuilder]))
 
 (defn with-elasticmq
@@ -15,7 +15,7 @@
       (tests))
     (.stopAndWait server)))
 
-(defn is-message-received
+(defn message-received?
   "Helper to assert that a message was received and matches the given pattern"
   [queue-name test-pat]
   (let [url (str "http://localhost:9324/queue/" queue-name)
@@ -28,27 +28,14 @@
 
 (deftest test-sqs
   (testing "ensure that logs get sent to SQS"
-    (timbre/set-config! [:appenders :sqs]
-                          (make-sqs-appender nil
-                                            {:queue-name "test"
-                                              :credential
-                                              {:access-key "sqs"
-                                               :secret-key ""
-                                               :endpoint "http://localhost:9324"}}))
+    (timbre/set-config! {:appenders {:sqs-appender (sqs-appender {:queue-name "test"})}})
+    (timbre/set-level! :debug)
     (timbre/error "A test message")
-    (is-message-received "test" #"A test message")))
+    (message-received? "test" #"A test message")))
 
 (deftest test-config
   (testing "no messages are sent if a queue is not configured"
-    (timbre/set-config! [:appenders :sqs] (make-sqs-appender))
+    (timbre/set-config! {:appenders {:sqs-appender (sqs-appender {})}})
+    (timbre/set-level! :debug)
     (timbre/error "A test message")
-    (is (= 0 (-> (sqs/list-queues) :queue-urls count))))
-  (comment "Not quite working yet..."
-           (testing "queue name can be configured dynamically"
-             (timbre/set-config! [:appenders :sqs] (make-sqs-appender))
-             (timbre/set-config! [:shared-appender-config :queue-name] "test")
-             (timbre/set-config! [:shared-appender-config :credential] {:access-key "sqs"
-                                                                        :secret-key ""
-                                                                        :endpoint "http://localhost:9324"})
-             (timbre/error "A test message")
-             (is-message-received "test" #"A test message"))))
+    (is (= 0 (-> (sqs/list-queues) :queue-urls count)))))
