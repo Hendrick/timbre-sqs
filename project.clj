73379883813dbf010d2667cccdb22388ed420c2b
Larry Staton Jr <larry@larrystaton.com>
2015-07-06 21:42:52 +0000

    Update Clojure to 1.7.0

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/Hendrick/timbre-sqs"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [amazonica "0.3.24"]
-                 [com.taoensso/timbre "4.0.1" :exclusions [org.clojure/tools.reader]]]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [amazonica "0.3.28"]
+                 [com.taoensso/timbre "4.0.2" :exclusions [org.clojure/tools.reader]]]
   :profiles
             {:dev
              {:dependencies [[org.elasticmq/elasticmq-server_2.11 "0.8.8" :exclusions [joda-time]]]}})
